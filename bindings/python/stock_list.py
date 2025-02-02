8e917f39276e960fc6383f47fa4920cbc0294d8f
pfspence <mspencer53@gmail.com>
2025-02-02 14:56:34 -0800

    fetch stock info

--- bindings/python/stock_list.py
+++ bindings/python/stock_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # Display a runtext with double-buffering.
 import random
-from samples.samplebase import SampleBase
 from rgbmatrix import graphics, RGBMatrix, RGBMatrixOptions
+import yfinance as yf
 import time
 
 
@@ -12,7 +12,6 @@ class StockList:
     def __init__(self, *args, **kwargs):
         options = RGBMatrixOptions()
 
-        # options.hardware_mapping = "adafruit-hat-pwm"
         options.rows = 32
         options.cols = 64
         options.chain_length = 1
@@ -40,12 +39,36 @@ class StockList:
     def get_change(self, ticker: str, interval: str = "day") -> float:
         return round(random.uniform(-20.0, 20.0), 1)
 
-    def get_ticker_changes(self, interval: str = "day") -> dict[str, float]:
-        tickers = self.get_tickers()
-        ticker_changes: dict[str, float] = {}
-        for ticker in tickers:
-            ticker_changes[ticker] = self.get_change(ticker, interval)
-        return ticker_changes
+    def get_ticker_data(self) -> list[tuple[str, tuple[int, int, int]]]:
+
+        lines = []
+        for ticker in self.get_tickers():
+            if " as " in ticker:
+                ticker, display_ticker = ticker.split(" as ")
+            else:
+                display_ticker = ticker
+
+            fast_info = yf.Ticker(ticker).fast_info
+            day_open = fast_info.open
+            last_price = fast_info.last_price
+            display_last_price = round(last_price)
+
+            day_change = round((last_price / day_open - 1) * 100, 1)
+            color = (0, 255, 0) if day_change > 0 else (255, 0, 0)
+            day_change = abs(day_change)
+            if day_change > 9.9:
+                day_change = round(day_change)
+
+            display_ticker = display_ticker.ljust(5)
+            display_last_price = f"${str(display_last_price)}".rjust(6)
+            display_change = f"{str(day_change)}%".rjust(5)
+
+            lines.append((f"{display_ticker}{display_last_price}{display_change}", color))  # tuple of text and color
+            time.sleep(1)
+
+        print(lines)
+        return lines
+
 
     def run(self):
         canvas = self.matrix.CreateFrameCanvas()
@@ -55,27 +78,21 @@ class StockList:
 
         while True:
             canvas.Clear()
-            ticker_changes = self.get_ticker_changes("day")
+            lines = self.get_ticker_data()
             y_pos = StockList.FONT_HEIGHT
             x_pos = 0
-            for ticker, change in ticker_changes.items():
 
-                text_color = graphics.Color(155, 255, 0) if change > 0 else graphics.Color(255, 0, 0)
-                change = abs(change)
-                if change < 1:
-                    change = "." + str(change)[-1]  # 0.1 -> .1
-                else:
-                    change = str(int(round(change)))  # 12.1 -> 12, 1.5 -> 1, 1.51 -> 2
-                graphics.DrawText(canvas, font, x_pos, y_pos, text_color, f"{ticker} {change}")
-
-                if x_pos == 0:
-                    x_pos = 32
-                elif x_pos == 32:
-                    x_pos = 0
+            batch_size = 5  # we can only display 5 lines at a time
+            for i in range(0, len(lines), batch_size):
+                batch = lines[i:i + batch_size]
+                for line, text_color in batch:
+                    graphics.DrawText(canvas, font, x_pos, y_pos, text_color, line)
                     y_pos += StockList.FONT_HEIGHT
 
-            canvas = self.matrix.SwapOnVSync(canvas)
-            time.sleep(5)
+                canvas = self.matrix.SwapOnVSync(canvas)
+                time.sleep(60)  # wait 60 seconds after displaying each batch
+                y_pos = StockList.FONT_HEIGHT  # reset y position for the next batch
+                canvas.Clear()
 
 # Main function
 if __name__ == "__main__":
